56a446702f9441f48a09aa2a7b61c044129e0160
Christian Daga <christian@korrelated.com>
2022-06-22 19:47:56 -0700

    display data

--- frontend/lib/widgets/map.dart
+++ frontend/lib/widgets/map.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/services.dart' show ByteData, rootBundle;
 
 class MapDisplay extends StatefulWidget {
   const MapDisplay({Key? key}) : super(key: key);
@@ -11,6 +14,7 @@ class MapDisplay extends StatefulWidget {
 
 class MapDisplayState extends State<MapDisplay> {
   final Completer<GoogleMapController> _controller = Completer();
+  Set<Marker>? _markers;
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.334465, -121.8812),
@@ -23,10 +27,12 @@ class MapDisplayState extends State<MapDisplay> {
 
   @override
   Widget build(BuildContext context) {
+    if (_markers == null) _fetchData();
     return Scaffold(
       body: GoogleMap(
         mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
+        markers: _markers ?? {},
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
@@ -42,5 +48,20 @@ class MapDisplayState extends State<MapDisplay> {
   Future<void> _goToTheLake() async {
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(_kSchool));
+  }
+
+  Future<void> _fetchData() async {
+    Set<Marker> newMarkers = {};
+    var data = await rootBundle.loadString('assets/sampleData.json');
+    var icon = await rootBundle.load('assets/images/phone.png');
+    var dec = json.decode(data);
+    dec.forEach((k,v) {
+      newMarkers.add(Marker(
+        markerId: MarkerId(k),
+        position: LatLng(v['lat']!, v['lng']!),
+        icon: BitmapDescriptor.fromBytes(Uint8List.view(icon.buffer))
+      ));
+    });
+    setState(() => _markers = newMarkers);
   }
 }
