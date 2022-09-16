288862913f0377f22881c6d8ad59d6120f12ac3f
christiandaga <christian.daga@sjsu.edu>
2022-09-16 12:07:40 -0700

    current location, permissions, and route boiler

--- frontend/lib/config.dart
+++ frontend/lib/config.dart
@@ -1,6 +1,12 @@
+import 'package:logger/logger.dart';
+import 'package:permission_handler/permission_handler.dart';
+
 class _Config {
   initConfig() async {
     // init stuff
+    await Permission.location.request();
+    logger.i(Permission.location);
   }
 }
 final _Config config = _Config();
+final Logger logger = Logger();
--- frontend/lib/widgets/map.dart
+++ frontend/lib/widgets/map.dart
@@ -2,8 +2,12 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/services.dart' show rootBundle;
+
+import '../config.dart';
 
 class MapDisplay extends StatefulWidget {
   const MapDisplay({Key? key}) : super(key: key);
@@ -13,13 +17,22 @@ class MapDisplay extends StatefulWidget {
 }
 
 class MapDisplayState extends State<MapDisplay> {
+
   final Completer<GoogleMapController> _controller = Completer();
   Set<Marker>? _markers;
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.334465, -121.8812),
-    zoom: 15.5,
-  );
+  late Position _currentPosition;
+  String _currentAddress = '';
+
+  final startAddressController = TextEditingController();
+  final destinationAddressController = TextEditingController();
+
+  final startAddressFocusNode = FocusNode();
+  final desrinationAddressFocusNode = FocusNode();
+
+  String _startAddress = '';
+  String _destinationAddress = '';
+  String? _placeDistance;
 
   static const CameraPosition _kSchool = CameraPosition(
       target: LatLng(37.334465, -121.8812),
@@ -27,28 +40,31 @@ class MapDisplayState extends State<MapDisplay> {
   );
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
     if (_markers == null) _fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        initialCameraPosition: _kSchool,
         markers: _markers ?? {},
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
       ),
       floatingActionButton: FloatingActionButton.small(
-        onPressed: _goToTheLake,
+        onPressed: _goToTheSchool,
         child: const Icon(Icons.school),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kSchool));
   }
 
   Future<void> _fetchData() async {
@@ -65,4 +81,50 @@ class MapDisplayState extends State<MapDisplay> {
     });
     setState(() => _markers = newMarkers);
   }
+
+  Future<void> _goToTheSchool() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kSchool));
+  }
+
+  Future<void> _getCurrentLocation() async {
+    final GoogleMapController controller = await _controller.future;
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+      .then((Position position) async {
+      setState(() {
+        _currentPosition = position;
+
+        logger.i('CURRENT POS: $_currentPosition');
+
+        controller.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: LatLng(position.latitude, position.longitude),
+              zoom: 18.0,
+            ),
+          ),
+        );
+      });
+      await _getAddress();
+    }).catchError((e) {
+      logger.e(e);
+    });
+  }
+
+  _getAddress() async {
+  try {
+    List<Placemark> p = await placemarkFromCoordinates(
+        _currentPosition.latitude, _currentPosition.longitude);
+    Placemark place = p[0];
+
+    setState(() {
+      _currentAddress =
+          "${place.name}, ${place.locality}, ${place.postalCode}, ${place.country}";
+      startAddressController.text = _currentAddress;
+      _startAddress = _currentAddress;
+    });
+  } catch (e) {
+    logger.e(e);
+  }
+}
 }
